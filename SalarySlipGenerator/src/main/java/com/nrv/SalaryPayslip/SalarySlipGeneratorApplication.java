144a577aa8fea3a3c27c0ab31e0af92f8e02b0f4
Karan Mohokar <karanmohokar7@gmail.com>
2024-08-21 15:35:29 +0530

    Spring Boot Initiased

---
+++ SalarySlipGenerator/src/main/java/com/nrv/SalaryPayslip/SalarySlipGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.nrv.SalaryPayslip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalarySlipGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalarySlipGeneratorApplication.class, args);
+	}
+
+}
---
+++ SalarySlipGenerator/src/test/java/com/nrv/SalaryPayslip/SalarySlipGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nrv.SalaryPayslip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalarySlipGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
